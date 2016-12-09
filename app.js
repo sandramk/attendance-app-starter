1d79288ca2a1d7b33f6eb5e56feacf58c3ea6f59
Sandra Killebrew <sandra@nextconsulting-llc.com>
2016-12-08 18:59:50 -0600

    having trouble with post route-javascript logic problematic

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
 //npm dependencies
 const express = require ('express');
-const bodyParser = require('body-parser');
 const path = require ('path');
 const ejs = require ('ejs');
 
 const app = express();
-
+const bodyParser = require('body-parser');
 
 //set our views directory
 app.use(express.static('assets'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-//add middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const names = require('./routes/names');
+//add routes
+const attendanceRoutes = require('./routes/names');
 
-app.use('/names', names);
+app.use('/names', attendanceRoutes);
 app.use('/*', function(req, res, next){
   res.redirect('/names');
 });
 
-const port = 3000;
+const port = 3060;
 app.listen(port, () => console.log(`Server listening on: ${port}`));
--- routes/names.js
+++ routes/names.js
@@ -1,36 +1,46 @@
 const express = require ('express');
 const router = express.Router();
 
-const people = [];
+//people is an associative array of objects, with properties name and number
+const people = [ {name: ' ', num: } ];
 
 router.get('/', function(req, res, next) {
-  res.render('names.ejs', { people: people
+//need to render both names and number
+  res.render('names.ejs', {
+    peopleRendered: people
   });
 });
+
+
+
+
+
+
 
 router.post('/', function(req, res, next) {
 //store input in newName variable
   const newName = req.body.name;
+
 let foundPerson;
-for(var i =0; i < people.length; i++) {
+console.log("making a post request");
+// for (var i = 0; i <= people.length; i++) {
+// console.log("people loop");
+
+  if (people.length === 0) {
+    foundPerson.name = newName;
+    foundPerson.num = 0;
+  }
+  //won't work without the loop
   if (people[i].name === newName) {
     foundPerson = people[i];
   }
-  if (!foundPerson){
+  if (!foundPerson) {
     foundPerson={name: newName, num: 0};
     people.push(foundPerson);
   }
   foundPerson.num += 1;
-}
+// }
 res.redirect('/names');
 });
 
-// for (i=0; i < people.length; i++) {
-//   if (newName === people[i]) {
-//     number = number + 1 ;
-//   } else {
-//     people.push({ name: newName, number: 1 });
-//
-//   }
-// }
 module.exports = router;
